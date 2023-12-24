56312d1ea9a2625a19dfc5517400f1e54d63cb23
Penguin Peps <112114400+PenguinPepper@users.noreply.github.com>
2023-12-24 17:04:21 +0000

    mend
    Set up tailwind and add basic logic for button component

--- web/src/components/menubar.jsx
+++ web/src/components/menubar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
 import Button from './button';
-import { UilPlusCircle } from '@iconscout/react-unicons';
+import { UilEstate } from '@iconscout/react-unicons'
 
 const Menubar = () => {
     return (
-        <menu>
+        <menu className='ml-5 mt-2'>
             <img src={logo} alt="logo" width={80} height={41}/>
             <ul className="list-none">
                 <li>
+                    <UilEstate className="inline-block mr-1" size={30} />
                     <a href="#home" className="">Home</a>
                 </li>
                 <li>
@@ -21,9 +22,8 @@ const Menubar = () => {
                     <a href="#analytics">Analytics</a>
                 </li>
             </ul>
-            <Button label="Transaction" intent="transaction" >
-                <UilPlusCircle size="300" color="#191C1B"/>
-            </Button>
+           
+            <Button label="Transaction" intent="transaction" />
         </menu>
     );
 };
