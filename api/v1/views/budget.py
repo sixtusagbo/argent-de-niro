094ae08654b0378a1e84137d7eeb0b6731426a26
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-01-06 07:43:04 +0000

    Ensure that only owner can update or delete budget

--- api/v1/views/budget.py
+++ api/v1/views/budget.py
@@ -6,7 +6,6 @@ from flask import Response, abort, jsonify, request
 from api.models.budget import Budget
 from api.models.user import User
 from api.v1.auth.auth_middleware import token_required
-from api.v1.auth.passwords import hash_password
 from api.v1.views import app_views
 from api.models.transaction import Transaction
 
@@ -108,11 +107,11 @@ def update_budget(current_user: User, budget_id: str = None) -> Response:
     Path params:
         - budget_id
     Form body:
-        - user_id (optional)
         - name (optional)
         - limit (optional)
         - start_date (optional)
         - end_date (optional)
+        - category_id (optional)
     Return:
         - Updated budget in JSON
         - 404 if the budget is not found
@@ -127,6 +126,8 @@ def update_budget(current_user: User, budget_id: str = None) -> Response:
         abort(404)
     if budget is None:
         abort(404)
+    if current_user.id != budget.user_id:
+        abort(403)
     payload = request.form
 
     # Update data
@@ -137,12 +138,12 @@ def update_budget(current_user: User, budget_id: str = None) -> Response:
             if Transaction.objects(budget_id=budget_id).count() > 0:
                 abort(400, "Cannot update limit on budget with transactions")
             budget.limit = payload.get("limit")
-        if "password" in payload:
-            budget.password = hash_password(payload.get("password"))
         if "start_date" in payload:
-            budget.birth_date = datetime.fromisoformat(payload.get("start_date")).date()
-        if "start_date" in payload:
-            budget.birth_date = datetime.fromisoformat(payload.get("start_date")).date()
+            budget.start_date = datetime.fromisoformat(payload.get("start_date")).date()
+        if "end_date" in payload:
+            budget.end_date = datetime.fromisoformat(payload.get("end_date")).date()
+        if "category_id" in payload:
+            budget.category_id = payload.get("category_id")
 
         budget.save()
     except Exception as e:
