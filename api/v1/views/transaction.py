c845f39de44e1be01264047dd32be0e5433a1979
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-01-06 08:26:48 +0000

    Add endpoint for transaction update

--- api/v1/views/transaction.py
+++ api/v1/views/transaction.py
@@ -67,3 +67,52 @@ def get_transaction(current_user: User, transaction_id: str) -> Response:
     if transaction.user_id != current_user.id:
         abort(403)
     return jsonify(json.loads(transaction.to_json()))
+
+
+@app_views.route("/transactions/<transaction_id>", methods=["PUT"])
+@token_required
+def update_transaction(current_user: User, transaction_id: str) -> Response:
+    """PUT /api/v1/transactions/<transaction_id>
+    Update a single transaction
+
+    Form body:
+      - category_id (optional)
+      - budget_id (optional)
+      - goal_id (optional)
+      - type (optional)
+      - amount (optional)
+      - date (optional)
+      - description (optional)
+
+    Return:
+      - Updated transaction in JSON
+      - 404 if transaction_id is not found
+      - 403 if transaction does not belong to current user
+      - 400 if transaction update fails
+    """
+    transaction = Transaction.objects(id=transaction_id).first()
+    if transaction is None:
+        abort(404)
+    if transaction.user_id != current_user.id:
+        abort(403)
+    payload = request.form
+
+    try:
+        if "category_id" in payload:
+            transaction.category_id = payload["category_id"]
+        if "budget_id" in payload:
+            transaction.budget_id = payload["budget_id"]
+        if "goal_id" in payload:
+            transaction.goal_id = payload["goal_id"]
+        if "type" in payload:
+            transaction.type = payload["type"]
+        if "amount" in payload:
+            transaction.amount = payload["amount"]
+        if "date" in payload:
+            transaction.date = payload["date"]
+        if "description" in payload:
+            transaction.description = payload["description"]
+        transaction.save()
+        return jsonify(json.loads(transaction.to_json()))
+    except Exception as e:
+        abort(400, e)
