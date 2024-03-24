c2aa574dcd1f33ddc25ae396180504c7b61b44f8
Penguin Peps <112114400+PenguinPepper@users.noreply.github.com>
2024-03-24 15:03:44 +0000

    Sumbit the data as a form and add datetime input

--- web/src/pages/signup.jsx
+++ web/src/pages/signup.jsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import Button from '../components/button';
 import { redirect } from 'react-router-dom';
 import ToggleEntry from '../components/toggleEntry';
-// import axios from 'axios';
+import axios from 'axios';
 
 
 const SignupPage = () => {
@@ -11,118 +11,173 @@ const SignupPage = () => {
     // const navigate = useNavigate();
 
     const { register, handleSubmit, formState: { errors } } = useForm();
+
     // const { errors } = formState;
 
-    const onSubmit = async (data) => {
-        // try {
-        //     const response = await axios.post('http://localhost:3000/api/v1/users', {
-        //         fullname: data.fullname,
-        //         email: data.email,
-        //         password: data.password,
-        //     });
+    const onSubmit = async (data, event) => {
+        const signupFormData = new FormData();
+        signupFormData.append('first_name', data.firstName);
+        signupFormData.append('last_name', data.surname);
+        signupFormData.append('email', data.email);
+        signupFormData.append('password', data.password);
 
-        //     console.log(response.data);
+        // Check if optional fields are filled out and append them to the form data
+        if (data.optionalField1) {
+            signupFormData.append('country', data.country);
+        }
+        if (data.optionalField2) {
+            signupFormData.append('timezone', data.timezone);
+        }
+        if (data.optionalField3) {
+            signupFormData.append('currency', data.currency);
+        }
+        try {
+            const response = await axios.post('http://localhost:5000/api/v1/users', signupFormData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
 
-        //     // Redirect to login page
-        //     window.location.href = '/login';
-        // } catch (error) {
-        //     console.error(error);
-        // }
-        console.log(response.data);
-        window.location.href = '/login';
+            console.log(response.data);
+            console.log("It went through");
+            event.preventDefault();
+            // // Redirect to login page
+            // window.location.href = '/login';
+        } catch (error) {
+            console.error(error);
+        }
+        // console.log(response.data);
+        // window.location.href = '/login';
     };
 
     return (
         // <section >
         //     <section className='w-10/12 h-10/12 rounded-r-xl'>
-        <section className='bg-[#90EB88] h-screen w-full flex items-center place-content-center'>
+        <section className='bg-[#90EB88] h-screen w-full '>
 
-            <section className=' mb-96'>
-                <ToggleEntry />
+            <button className='bg-white rounded mt-16 max-sm:ml-20 sm:ml-44 py-2 px-12'>log in</button>
+            <button className='bg-white rounded py-2 px-12'>sign up</button>
+
+            <section className='bg-[#90EB88]  flex items-center justify-center'>
+
+                <form onSubmit={handleSubmit(onSubmit)} >
+                    <section className='mt-8 grid grid-cols-1 gap-x-4 min-md:gap-y-8 md:grid-cols-6'>
+
+                        <label htmlFor='firstName' className='block font-medium leading-6 text-gray-900'>
+                            <input
+                                className='bg-white rounded-2xl pl-8 pr-8 py-2 my-3 w-full outline-none'
+                                type='text'
+                                name='firstName'
+                                placeholder='First Name'
+                                {...register('firstName',
+                                    {
+                                        required: {
+                                            value: true,
+                                            message: "First Name is required",
+                                        },
+                                        minLength: {
+                                            value: 2,
+                                            message: "First Name must be at least 2 characters",
+                                        },
+                                    })}
+                            />
+                        </label>
+                        {/* <span className='hidden sm:inline-block text-sm ml-3 my-0 text-red-600'>{errors.fullname?.message}</span> */}
+
+
+                        <label htmlFor='surname' className='block font-medium leading-6 text-gray-900'>
+                            <input
+                                className='bg-white rounded-2xl pl-8 pr-8 py-2 my-3 w-full outline-none'
+                                type='text'
+                                name='surname'
+                                placeholder='Surname'
+                                {...register('surname',
+                                    {
+                                        required: {
+                                            value: true,
+                                            message: "Surname is required",
+                                        },
+                                        minLength: {
+                                            value: 2,
+                                            message: "Surname must be at least 2 characters",
+                                        },
+                                    })}
+                            />
+
+                            <label htmlFor='dateOfBirth' className='block font-medium leading-6 text-gray-900'>
+                                <input
+                                    type='datetime-local'
+                                    name='dateOfBirth'
+                                    {...register('dateOfBirth',
+                                        {
+                                            required: {
+                                                value: true,
+                                                message: "Date of Birth is required",
+                                            },
+                                        })}
+                                />
+                            </label>
+
+                        </label>
+                        {/* <span className='hidden sm:inline-block text-sm ml-3 my-0 text-red-600'>{errors.fullname?.message}</span> */}
+                    </section>
+
+                    <label htmlFor='email' className='font-medium leading-6 text-gray-900'>
+                        <input
+                            className='bg-white rounded-2xl pl-8 py-2 my-2 w-full outline-none'
+                            type='email'
+                            name='email'
+                            placeholder='Email'
+                            {...register('email',
+                                {
+                                    required: {
+                                        value: true,
+                                        message: "Email address is required",
+                                    },
+                                    pattern: {
+                                        value:
+                                            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                        message: "Invalid email format"
+                                    },
+                                })}
+                        />
+                        {/* <span className='text-sm ml-3 text-red-600'>{errors.email?.message}</span> */}
+                    </label>
+
+                    <label htmlFor='password' className='font-medium leading-6 text-gray-900'>
+                        <input
+                            className='bg-white rounded-2xl pl-8 py-2 my-2 w-full outline-none'
+                            type='password'
+                            name='password'
+                            placeholder='Password'
+                            {...register('password',
+                                {
+                                    required: {
+                                        value: true,
+                                        message: "Password is required",
+                                    },
+                                    minLength: {
+                                        value: 8,
+                                        message: "Password must be at least 8 characters",
+                                    },
+                                    pattern: {
+                                        value:
+                                            /^(?=.*[A-Z])(?=.*[!@#$&*^%()~`])(?=.*[0-9]).{8,}$/,
+                                        message: "Must contain: 1 special charceter, a capital letter and a number"
+                                    },
+                                })}
+                        />
+                        {/* <span className='text-sm ml-3 text-red-600'>{errors.password?.message}</span> */}
+                    </label>
+                    <section className='flex flex-col mx-16 ml-28 sm:ml-48'>
+                        <Button intent='welcoming' label='Sign Up' />
+                    </section>
+                    <p className='text-center'>Already have an acccount? Login</p>
+
+                </form>
             </section>
 
-            <form onSubmit={handleSubmit(onSubmit)} >
-                <label htmlFor='fullname'>
-                    <input
-                        className='bg-white rounded-2xl pl-8 py-2 my-3 w-full outline-none'
-                        type='text'
-                        name='fullname'
-                        placeholder='Full Name'
-                        {...register('fullname',
-                            {
-                                required: {
-                                    value: true,
-                                    message: "Full name is required",
-                                },
-                                minLength: {
-                                    value: 2,
-                                    message: "Full name must be at least 2 characters",
-                                },
-                                maxLength: {
-                                    value: 30,
-                                    message: "Full name must not exceed 30 characters",
-                                },
-                            })}
-                    />
-
-                </label>
-                {/* <span className='hidden sm:inline-block text-sm ml-3 my-0 text-red-600'>{errors.fullname?.message}</span> */}
-
-                <label htmlFor='email'>
-                    <input
-                        className='bg-white rounded-2xl pl-8 py-2 my-2 w-full outline-none'
-                        type='email'
-                        name='email'
-                        placeholder='Email'
-                        {...register('email',
-                            {
-                                required: {
-                                    value: true,
-                                    message: "Email address is required",
-                                },
-                                pattern: {
-                                    value:
-                                        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                                    message: "Invalid email format"
-                                },
-                            })}
-                    />
-                    {/* <span className='text-sm ml-3 text-red-600'>{errors.email?.message}</span> */}
-                </label>
-                <label htmlFor='password'>
-                    <input
-                        className='bg-white rounded-2xl pl-8 py-2 my-2 w-full outline-none'
-                        type='password'
-                        name='password'
-                        placeholder='Password'
-                        {...register('password',
-                            {
-                                required: {
-                                    value: true,
-                                    message: "Password is required",
-                                },
-                                minLength: {
-                                    value: 8,
-                                    message: "Password must be at least 8 characters",
-                                },
-                                pattern: {
-                                    value:
-                                        /^(?=.*[A-Z])(?=.*[!@#$&*^%()~`])(?=.*[0-9]).{8,}$/,
-                                    message: "Must contain: 1 special charceter, a capital letter and a number"
-                                },
-                            })}
-                    />
-                    {/* <span className='text-sm ml-3 text-red-600'>{errors.password?.message}</span> */}
-                </label>
-                <section className='flex flex-col mx-16 ml-28 sm:ml-48'>
-                    <Button intent='welcoming' label='Sign Up' />
-                </section>
-                <p className='text-center'>Already have an acccount? Login</p>
-
-            </form>
         </section>
-
-        //     </section>
         // </section>
     );
 }
