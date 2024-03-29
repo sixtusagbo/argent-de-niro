742c16ba9a0a14932c06c6b14f86f0d3aa56bf06
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-03-29 05:23:45 +0000

    Add unit tests for Category model

---
+++ api/tests/test_models/test_category.py
@@ -0,0 +1,39 @@
+import unittest
+from api.models import test_connect, test_disconnect
+from api.models.category import Category
+from mongoengine.errors import ValidationError
+from api.tests import test_category
+
+
+class TestCategory(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        test_connect()
+
+    @classmethod
+    def tearDownClass(cls):
+        test_disconnect()
+
+    def setUp(self):
+        self.category = test_category
+        self.category.save()
+
+    def tearDown(self):
+        Category.objects().delete()
+
+    def test_required_fields(self):
+        with self.assertRaises(ValidationError):
+            Category().save()
+
+    def test_to_json(self):
+        json_data = self.category.to_json()
+        self.assertIsInstance(json_data, str)
+
+    def test_to_dict(self):
+        dict_data = self.category.to_dict()
+        self.assertIsInstance(dict_data, dict)
+        self.assertEqual(dict_data["name"], "Food")
+
+
+if __name__ == "__main__":
+    unittest.main()
