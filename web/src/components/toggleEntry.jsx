5da5900f9d1a651992f9a890645d08ca89afca13
Penguin Peps <112114400+PenguinPepper@users.noreply.github.com>
2024-04-02 09:50:18 +0000

    Add the toggle entry as a list

--- web/src/components/toggleEntry.jsx
+++ web/src/components/toggleEntry.jsx
@@ -18,20 +18,10 @@ const ToggleEntry = () => {
 
 
     return (
-        <section className='bg-white rounded-2xl absolute w-screen sm:w-1/2'>
-            <p
-                className={`inline-block w-1/2 text-black py-3 text-center rounded-2xl hover:cursor-pointer  hover:bg-emerald-100 active:bg-emerald-400 bg-white`}
-                onClick={() => handleArticleClick('login')}
-            >
-                log in
-            </p>
-            <p
-                className={`inline-block w-1/2 text-black py-3 text-center rounded-2xl hover:cursor-pointer hover:bg-emerald-100 active:bg-emerald-400 bg-white`}
-                onClick={() => handleArticleClick('signup')}
-            >
-                sign up
-            </p>
-        </section>
+        <ul className='list-none p-0 mt-10 mb-0 ml-4 mr-0 after:table after:clear-both flex flex-row justify-center'>
+            <li><a href='/signup' className='p-2 px-12 bg-white cursor-pointer no-underline text-xl text-center text-gray-800 block float-left hover:bg-emerald-100 rounded-l-md focus:bg-emerald-400'>sign up</a></li>
+            <li><a href='/login' className='p-2 px-12 bg-white cursor-pointer no-underline text-xl text-center text-gray-800 block float-left hover:bg-emerald-100 rounded-r-md focus:bg-emerald-400'>log in</a></li>
+        </ul>
     );
 };
 
